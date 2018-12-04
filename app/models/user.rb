68dfe9295fb0cb9eb56a5ddc6a8ec44fcb74f4f2
andreydro <andreydro14@gmail.com>
2018-12-04 18:38:32 +0300

    authorize api request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_secure_password
 
   has_many :todos, foreign_key: :created_by
--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AuthorizeApiRequest do
+  # Create test user
   let(:user) { create(:user) }
+  # Mock `Authorization` header
   let(:header) { { 'Authorization' => token_generator(user.id) } }
-
+  # Invalid request subject
   subject(:invalid_request_obj) { described_class.new({}) }
+  # Valid request subject
   subject(:request_obj) { described_class.new(header) }
 
+  # Test Suite for AuthorizeApiRequest#call
+  # This is our entry point into the service class
   describe '#call' do
+    # returns user object when request is valid
     context 'when valid request' do
       it 'returns user object' do
         result = request_obj.call
@@ -15,6 +21,7 @@ RSpec.describe AuthorizeApiRequest do
       end
     end
 
+    # returns error message when invalid request
     context 'when invalid request' do
       context 'when missing token' do
         it 'raises a MissingToken error' do
@@ -25,10 +32,11 @@ RSpec.describe AuthorizeApiRequest do
 
       context 'when invalid token' do
         subject(:invalid_request_obj) do
+          # custom helper method `token_generator`
           described_class.new('Authorization' => token_generator(5))
         end
 
-        it 'raises an InvalidToken' do
+        it 'raises an InvalidToken error' do
           expect { invalid_request_obj.call }
             .to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
         end
